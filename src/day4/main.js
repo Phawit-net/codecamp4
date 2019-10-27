8aff9251ca382d17154c9ada1e43ada03e918767
Phawit-net <neztle.net@gmail.com>
2019-10-27 23:33:35 +0700

    day4

--- src/day4/main.js
+++ src/day4/main.js
@@ -1,3 +1,17 @@
-export function main(arr) {
-	// Your code begins here;
+function main(arr) {
+	console.log(typeof(arr));
+	console.log(arr[1][1]);
+	count = 1;
+	for (let i=0;i<arr.length;i++)
+	{
+		console.log(i)
+		console.log(typeof(arr))
+			if(arr[i][1] > (arr[i+1][1]))
+			{
+				count ++;
+			}
+		
+	}
+	//console.log(count);
 }
+main([[30, 75], [0, 50], [60, 150], [45, 50]])

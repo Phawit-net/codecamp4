5f196b53ed3871cf8a87495d7536217ba15dcfc8
Phawit-net <neztle.net@gmail.com>
2019-10-28 23:11:03 +0700

    fix

--- src/day4/main.js
+++ src/day4/main.js
@@ -1,15 +1,11 @@
 function main(arr) {
-	console.log(typeof(arr));
-	console.log(arr[1][1]);
 	count = 1;
 	for (let i=0;i<arr.length;i++)
 	{
-		console.log(i)
-		console.log(typeof(arr))
-			if(arr[i][1] > (arr[i+1][1]))
-			{
-				count ++;
-			}
+		if(arr[i][1] > (arr[i+1][1]))
+		{
+			count ++;
+		}
 		
 	}
 	//console.log(count);

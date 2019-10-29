a8b26f70b3acc053038b6a4a4c076234f695b70e
Phawit-net <neztle.net@gmail.com>
2019-10-29 09:08:44 +0700

    change

--- src/day4/main.js
+++ src/day4/main.js
@@ -1,17 +1,13 @@
 function main(arr) {
-	console.log(typeof(arr));
-	console.log(arr[1][1]);
 	count = 1;
-	for (let i=0;i<arr.length;i++)
+	for (let i=0;i<arr.length-1;i++)
 	{
-		console.log(i)
-		console.log(typeof(arr))
 			if(arr[i][1] > (arr[i+1][1]))
 			{
 				count ++;
 			}
 		
 	}
-	//console.log(count);
+	console.log(count);
 }
-main([[30, 75], [0, 50], [60, 150], [45, 50]])
+main([[30, 75], [80, 120], [60, 150], [45, 50]])

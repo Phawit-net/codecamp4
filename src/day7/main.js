dc8327ce44f8d6d34d37d62c35332a66f29db1c0
Phawit-net <neztle.net@gmail.com>
2019-11-03 22:51:36 +0700

    update

--- src/day7/main.js
+++ src/day7/main.js
@@ -1,5 +1,45 @@
-function main(arr) {
-	// Your code begins here;
-}
+// function main(arr) {
+// 	let result = []
+// 	// result.push([])
+// 	// for(let i =0;i<arr.length;i++)
+// 	// {
+// 	// 	result.push([arr[i]])
+		
+// 	// 	for(let j =i+1;j<arr.length;j++)
+// 	// 	{
+// 	// 		let a = []
+// 	// 		a.push(arr[i])
+// 	// 		a.push(arr[j])
+// 	// 		result.push(a)
+// 	// 	}
+		
+// 	// }
+// 	// result.push(arr)
+// 	// console.log(result)
+// 	for(let i=0;i<=arr.length;i++)
+// 	{
+// 		for(let j=i+1;j<=arr.length;j++)
+// 		{ 
+// 			result.push(arr.slice(i,j))
+// 		}
+// 	}
+// 	console.log(result)
+// }
 
-module.exports = { main };
+// main([1, 2, 3, 4])
+// module.exports = { main };
+
+
+var a = [1, 2, 3,4], res = [];
+for (var i = 0; i < Math.pow(2, a.length); i++) {
+    var bin = (i).toString(2), set = [];
+    bin = new Array((a.length-bin.length)+1).join("0")+bin;
+    console.log(bin);
+    for (var j = 0; j < bin.length; j++) {
+        if (bin[j] === "1") {
+            set.push(a[j]);
+        }
+    }
+    res.push(set);
+}
+console.log(res);
--- src/day8/main.js
+++ src/day8/main.js
@@ -1,5 +1,15 @@
-function main() {
-	// Your code begins here;
+function main(arr,...num) {
+	for(let i =0;i<arr.length;i++)
+	{
+		for(let j=0;j<num.length;j++)
+		{
+			if(arr[i] === num[j])
+			{
+				arr.splice(i,1)
+				i=i-1
+			}
+		}
+	}
+	return (arr)
 }
-
 module.exports = { main };
